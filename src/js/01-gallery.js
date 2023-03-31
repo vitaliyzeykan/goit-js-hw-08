e32727ce4447550cf0a89f4d18f34b4063d8c14e
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-03-31 10:21:05 +0300

    #1 and #3 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesEl = document.querySelector('ul.gallery');
+imagesEl.style.listStyle = 'none';
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+            <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            </li>
+        `
+    )
+    .join('');
+}
+
+const addGalleryMarkyp = createGalleryMarkup(galleryItems);
+
+imagesEl.innerHTML = addGalleryMarkyp;
+
+imagesEl.addEventListener('click', onGalleryItemClick);
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onGalleryItemClick(evt) {
+  blockStandartAction(evt);
+
+  const isGalleryItemEl = evt.target.classList.contains('gallery__image');
+  if (!isGalleryItemEl) {
+    return;
+  }
+
+  console.log(evt.target);
+
+  gallery.open(isGalleryItemEl);
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 1000));
+
+fillTextarea();
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem('STORAGE_KEY', formDataJSON);
+}
+
+function fillTextarea() {
+  const savedForm = localStorage.getItem('STORAGE_KEY');
+  const parsedSavedForm = JSON.parse(savedForm);
+  if (parsedSavedForm) {
+    refs.email.value = parsedSavedForm.email;
+    refs.textarea.value = parsedSavedForm.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(localStorage.getItem('STORAGE_KEY'));
+  e.currentTarget.reset();
+  localStorage.removeItem('STORAGE_KEY');
+}
